cc1571bc3e5414ef8dcf869e38534425153a93d1
bikramshankhar <56664732+bikramshankhar@users.noreply.github.com>
2020-05-04 22:36:20 -0500

    commit

---
+++ server.js
@@ -0,0 +1,33 @@
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var express = require('express');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(methodOverride('_method'));
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controllers.js');
+app.use('/', routes);
+
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+ 
